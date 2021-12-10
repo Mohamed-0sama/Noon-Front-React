62f572b20804a8d82a4c40e3f0f191c3f888b029
Abdelmomen-Nasreldin <92486538+Abdelmomen-Nasreldin@users.noreply.github.com>
2021-12-10 19:48:13 +0200

    before merge +

--- src/components/User/User components/Data.jsx
+++ src/components/User/User components/Data.jsx
@@ -1,5 +1,5 @@
 // import React from 'react'
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Layout, Menu } from "antd";
 import {
   MenuUnfoldOutlined,
@@ -11,46 +11,103 @@ import {
   // UploadOutlined,
 } from "@ant-design/icons";
 import { Link } from "react-router-dom";
-import { Outlet } from 'react-router';
+import { Outlet } from "react-router";
 
 const { Header, Sider, Content } = Layout;
 // import "{ant-tooltip-inner}"
 const Data = () => {
-     const [collapsed, setCollapsed] = useState(false);
+  const [widowWidth, setWindowWidth] = useState(() => {
+    return window.innerWidth;
+  });
+  const siderRef = useRef();
+  const sideControllerrSMRef = useRef();
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      setWindowWidth(window.innerWidth);
+    });
+  }, [widowWidth]);
+  const [collapsed, setCollapsed] = useState(true);
   const [name, setName] = useState("");
   const toggle = () => {
     console.log(!collapsed);
-    setCollapsed(!collapsed);
+
+    if (widowWidth <= 576) {
+      setCollapsed(true);
+      siderRef.current.style.marginLeft = -80
+      if (siderRef.current.style.marginLeft === "-80px") {
+        siderRef.current.style.marginLeft = "0px"
+      }else {
+        siderRef.current.style.marginLeft = "-80px"
+      }
+      // sideControllerrSMRef.current.style.
+    } else {
+      setCollapsed(!collapsed);
+    }
   };
-  const componentName = (name)=>{
-    setName(name)
-  }
-    return (
-        <div className="userData">
-            <Layout style={{minHeight:" 70vh"}}>
-        <Sider className="sider" trigger={null} collapsible collapsed={collapsed}>
+  const componentName = (name) => {
+    setName(name);
+  };
+  return (
+    <div className="userData">
+      <Layout style={{ minHeight: " 70vh" }}>
+        <Sider
+          className="sider"
+          trigger={null}
+          collapsible
+          collapsed={collapsed}
+          ref={siderRef}
+        >
           <div className="logo" />
-          <Menu className="sider" theme="light" mode="inline" defaultSelectedKeys={["1"]}>
-          <Menu.Item key="1" icon={<ShopOutlined />} onClick={()=>componentName("Order")}>
+          <Menu
+            className="sider"
+            theme="light"
+            mode="inline"
+            defaultSelectedKeys={["1"]}
+          >
+            <Menu.Item
+              key="1"
+              icon={<ShopOutlined />}
+              onClick={() => componentName("Order")}
+            >
               <Link to="/User/order">Order</Link>
             </Menu.Item>
-            <Menu.Item key="2" icon={<HomeOutlined />} onClick={()=>componentName("Address")}>
+            <Menu.Item
+              key="2"
+              icon={<HomeOutlined />}
+              onClick={() => componentName("Address")}
+            >
               <Link to="/User/address">Address</Link>
             </Menu.Item>
-            
-            <Menu.Item key="3" icon={<UserOutlined />} onClick={()=>componentName("Profile")}>
+
+            <Menu.Item
+              key="3"
+              icon={<UserOutlined />}
+              onClick={() => componentName("Profile")}
+            >
               <Link to="/User/profile">Profile</Link>
             </Menu.Item>
           </Menu>
         </Sider>
         <Layout className="site-layout">
-          <Header className="site-layout-background icon-header" style={{ padding: 0 }}>
-              
+          <Header
+            className="site-layout-background icon-header"
+            style={{ padding: 0 }}
+          >
             {React.createElement(
               collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
               {
-                className: "trigger",
+                className: "trigger d-none d-sm-block",
                 onClick: toggle,
+
+              }
+            )}
+            {React.createElement(
+              collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
+              {
+                className: "trigger d-block d-sm-none",
+                onClick: toggle,
+                ref: sideControllerrSMRef
+
               }
             )}
             <h2>{name}</h2>
@@ -64,12 +121,12 @@ const Data = () => {
               minHeight: 280,
             }}
           >
-            <Outlet/>
+            <Outlet />
           </Content>
         </Layout>
       </Layout>
-        </div>
-    )
-}
+    </div>
+  );
+};
 
-export default Data
+export default Data;
